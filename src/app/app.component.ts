68028d95f3df9f2166c281d08aeb76124ad35412
Hritik <hritikkanojiya13@gmail.com>
2021-11-08 09:59:29 +0530

    String Interpolation, Property Binding, Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  messageFlag = false;
+  eventBindMessage = '';
 
+  constructor() {
+    // Property Binding for Disabled attribute
+    setTimeout(() => {
+      this.messageFlag = true;
+    }, 2500);
+  }
+
+  // String Interpolation with a Property
+  strInterProperty = 'String interpolation using a Property';
+
+  // String Interpolation with a Method
+  strInterMethod = 'String interpolation using a Method';
+  getstrInterMethod() {
+    return this.strInterMethod;
+  }
+
+  // Event Binding
+  showEventBindMessage(){
+    this.eventBindMessage = "Onclick Event Occured. Event Binding successful !!!";
+    this.messageFlag = false;
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+  // Selectors can be also referenced by using classes { .app-navbar } OR attributes { [app-navbar] }, above we are referencing selectors directly by the element.
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
